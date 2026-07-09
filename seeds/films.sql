70235321aa2c54bcb5c00223f5f740f83f40ee36
naimaahsan <142057464+naimaahsan@users.noreply.github.com>
2026-07-10 00:52:10 +0100

    film repo test fixed

--- seeds/films.sql
+++ seeds/films.sql
@@ -13,8 +13,8 @@ CREATE TABLE films (
 TRUNCATE table films;
 -- Finally, we add any records that are needed for the tests to run
 INSERT INTO films (title, genre, image_url) VALUES 
-('Avatar', 'Sci-Fi', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-DLojKo3wqs8yTacNuM9-SRoq9kikRCKlWSHigdRkDw&s=10'),
-('Titanic', 'Romance', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuIpILxfxzzRPddxcQJYvDsZoVPy4JW6ee4gL2Z9EUKA&s=10'),
-('Jurassic Park', 'Adventure', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH5kHgp-FzYkloVC41KyO8IXJMlDOrRImK_PunDQ-oJg&s=10)'),
+('Avatar', 'Sci-Fi', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM7DgT4oRUhxad_joaWTkQPmrttgcEs2yUAwZybkFKBQ&s=10'),
+('Titanic', 'Romance', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnH7fCIsviy1SUp9eibv2XhADZXR2Oi9PfYqYowtJhnQ&s=10'),
+('Jurassic Park', 'Adventure', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqe37l0ma2XnZSwn9UcW0WmZ6CSQJmJoaVMHeTBu8XeA&s=10'),
 ('Frozen', 'Animation', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh345oRxXqgUIHRC4Adai667S_SwMxArM4DNatiH4nXw&s=10)'),
-('The Dark Knight Rises', 'Action', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq6_VhuC-AK-XpAWdL-6mMEt6hoVPilbU2aLRsicBXng&s=10');
+('The Dark Knight Rises', 'Action', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRaZRYPjOrMkDMouLK0f6MvX2B6NHjv1frx_fjidpOYA&s=10');
